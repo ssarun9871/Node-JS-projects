39f4426a48f0df636b69ad14ef2a22316a78c18a
ssarun9871 <ssarun9871@gmail.com>
2022-12-17 01:29:08 +0530

    routing requests

--- first-app.js
+++ first-app.js
@@ -1,7 +1,34 @@
 const http = require('http');
 
 const server=http. createServer((req,res)=>{
- console.log('arun');
+ let url = req.url; 
+
+ if(url=='/home'){
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Welcome Home</h1></body>');
+    res.write('</html>');
+    res.end();
+ }
+
+ else if(url =='/about'){
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Welcome to about us page</h1></body>');
+    res.write('</html>');
+    res.end();
+ }
+
+ else if(url =='/node'){
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Welcome to my node js project</h1></body>');
+    res.write('</html>');
+    res.end();
+ }
+ 
+
+ 
 });
 
 server.listen(4000);
