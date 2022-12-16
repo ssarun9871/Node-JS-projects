055197075b38f5107c9bc9e00870338a1e84fef9
ssarun9871 <ssarun9871@gmail.com>
2022-12-17 02:58:06 +0530

    read/write file

--- first-app.js
+++ first-app.js
@@ -1,9 +1,42 @@
+const fs = require('fs');
 const http = require('http');
 
 const server=http. createServer((req,res)=>{
- let url = req.url; 
+ const url = req.url; 
+ const method = req.method;
+ if(url=='/'){
 
- if(url=='/home'){
+    fs.readFile('message.txt','utf-8',(err,data)=>{
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write(`<body><p>${data}</p><form action="/message" method="POST"><input type="text" name="message"><button type ="submit">Send</button></form></body>`);
+        res.write('</html>');
+        return res.end();
+    })
+   
+ }
+ 
+if(url== '/message' && method=='POST'){
+    const body =[];
+    req.on('data',(chunk)=>{
+        body.push(chunk);
+    })
+
+    return req.on('end',()=>{
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFile('message.txt',message,err=>{
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+
+            return res.end();
+            
+        });
+    });
+    }
+ 
+    
+ else if(url=='/home'){
     res.write('<html>');
     res.write('<head><title>My First Page</title><head>');
     res.write('<body><h1>Welcome Home</h1></body>');
