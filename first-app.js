54b9610b58df4cc25525198ecc3e0767fda2129d
ssarun9871 <ssarun9871@gmail.com>
2022-12-19 11:49:42 +0530

    task 4 (using router)

--- first-app.js
+++ first-app.js
@@ -3,21 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-
-app.use('/add-product',(req,res,next)=>{
-
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</button></form>');
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>')
 })
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>This is home</h1>');
-})
 const server=http. createServer(app);
 app.listen(3000);
