0bb2e77699ad96c95c4bfb575ce174fd4dca55df
ssarun9871 <ssarun9871@gmail.com>
2022-12-21 03:19:40 +0530

    serving html, css and JS

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const rootDir = require('../util/path');
 
 router.get('/add-product',(req,res,next)=>{
 
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 })
 
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
 })
 
-module.exports = router; 
+module.exports = router;  
--- routes/contactUs.js
+++ routes/contactUs.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path');
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+router.get('/contactus',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contactUs.html'));
 })
 
-module.exports = router; 
+module.exports = router;
